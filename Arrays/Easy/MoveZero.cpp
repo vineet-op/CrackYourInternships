702ae08e88ae1d1c0563bb08419c8ab280c65782
Vineet Jadhav <vineet290403@gmail.com>
2023-07-04 14:33:10 +0530

    leetcode

--- Arrays/Easy/MoveZero.cpp
+++ Arrays/Easy/MoveZero.cpp
@@ -1,3 +1,5 @@
+//
+//* Explanantion: iN THIS  CODE I HAVE USED 2 POINTER APPROCH TO COMPARE  FIRST AND SECOND ELEMEMTS AND MAKE SWAP
 #include <iostream>
 using namespace std;
 
@@ -33,3 +35,20 @@ int main()
         cout << "  " << arr[i];
     }
 }
+
+/*
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+
+        for(int j = 0; j < nums.size(); j++){
+            if(nums[j] != 0){
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
+    }
+};
+*/
